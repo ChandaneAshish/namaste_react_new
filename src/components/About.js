f68684d288a57b4109592f64a7a42133e504ad96
ChandaneAshish <chandaneashish145@gmail.com>
2025-04-03 22:34:30 +0530

    episode-08

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import User from "./User";
+import UserClass from "./UserClass";
 
 const About = () => {
   return (
@@ -6,6 +7,7 @@ const About = () => {
       <h1>About Us</h1>
       <h2>This is about u page of react webseries</h2>
       <User />
+      <UserClass />
     </div>
   );
 };
---
+++ src/components/UserClass.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class UserClass extends React.Component {
+  render() {
+    return (
+      <div className="user-card">
+        <h2>Name: Ashish</h2>
+        <h3>Location: Pune</h3>
+        <h3>Contact: 9876543210</h3>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
