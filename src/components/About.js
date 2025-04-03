9a0fba40a66e489dd775860d5f617f4345ed8e56
ChandaneAshish <chandaneashish145@gmail.com>
2025-04-03 22:00:19 +0530

    episode-08

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import User from "./User";
+
+const About = () => {
+  return (
+    <div className="about">
+      <h1>About Us</h1>
+      <h2>This is about u page of react webseries</h2>
+      <User />
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = () => {
+  return (
+    <div className="user-card">
+      <h2>Name: Ashish</h2>
+      <h3>Location: Pune</h3>
+      <h3>Contact: 9876543210</h3>
+    </div>
+  );
+};
+
+export default User;
