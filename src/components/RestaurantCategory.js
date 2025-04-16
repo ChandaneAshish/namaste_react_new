58bd34760a577d8da4194e65b77167fd66fceefe
ChandaneAshish <chandaneashish145@gmail.com>
2025-04-16 22:59:46 +0530

    Episode 11

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,11 +1,24 @@
-const RestaurantCategory = ({ data }) => {
-  console.log(data);
+import { useState } from "react";
+import ItemList from "./ItemList";
+
+const RestaurantCategory = ({ data, showItems, setShowIndex }) => {
+  // const [showItem, setShowItem] = useState(false);
+  // const handleClick = () => {
+  //   setShowItem(!showItem);
+  // };
   return (
     <div>
       {/* Accordian Header */}
-      <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg flex justify-between">
-        <span>{data.title}</span>
-        <span>⬇️</span>
+      <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg ">
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={handleClick}>
+          <span className="font-bold text-lg">
+            {data.title}: ({data.itemCards.length})
+          </span>
+          <span>⬇️</span>
+        </div>
+        {showItems && <ItemList items={data.itemCards} />}
       </div>
 
       {/* Accordian Body */}
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,15 +1,18 @@
 //import { useEffect, useState } from "react";
-import Shimmer from './Shimmer';
-import { MENU_API } from '../utils/constants';
+import Shimmer from "./Shimmer";
+import { MENU_API } from "../utils/constants";
 
-import { useParams } from 'react-router-dom';
-import useRestaurantMenu from '../utils/useRestaurantMenu';
-import RestaurantCategory from './RestaurantCategory';
+import { useParams } from "react-router-dom";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
+import { useState } from "react";
 
 const RestaurantMenu = () => {
   //const [resInfo, setResInfo] = useState(null);
 
   const { resId } = useParams();
+
+  const [showIndex, setShowIndex] = useState(0);
 
   //console.log(resId);
 
@@ -45,8 +48,8 @@ const RestaurantMenu = () => {
   const categories =
     resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
       (c) =>
-        c.card?.card?.['@type'] ==
-        'type.googleapis.com/swiggy.presentation.food.v2.ItemCategory'
+        c.card?.card?.["@type"] ==
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     );
 
   //In the above code we write ["@type"] as @ property is not variable and we can not write it directly so we need enclose it in [""]
@@ -54,10 +57,10 @@ const RestaurantMenu = () => {
   //console.log(categories);
 
   return (
-    <div className='text-center'>
-      <h1 className='my-6 font-bold text-2xl'>{name}</h1>
-      <h3 className='text-lg font-bold'>
-        {cuisines.join(', ')} - {costForTwoMessage}
+    <div className="text-center">
+      <h1 className="my-6 font-bold text-2xl">{name}</h1>
+      <h3 className="text-lg font-bold">
+        {cuisines.join(", ")} - {costForTwoMessage}
       </h3>
       {/*<h2>Menu</h2>
        <ul> */}
@@ -73,8 +76,13 @@ const RestaurantMenu = () => {
       </ul> */}
 
       {/* Building categories accordian UI */}
-      {categories.map((category) => (
-        <RestaurantCategory data={category?.card?.card} />
+      {categories.map((category, index) => (
+        <RestaurantCategory
+          key={category?.card?.card.title}
+          data={category?.card?.card}
+          showItems={index === showIndex ? true : false}
+          setShowIndex={() => setShowIndex(index)}
+        />
       ))}
     </div>
   );
